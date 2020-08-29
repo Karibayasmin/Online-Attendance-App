e5a97cdcce194870c8f3f74c4a3e2f040066407d
Your NameKariba <you@example.comkaribayasmin@gmail.com>
2020-08-29 15:19:09 +0600

    Partially completed Project

--- app/src/main/java/com/elegantwizardry/onlineattendanceapp/AttendanceInfoSubmit.java
+++ app/src/main/java/com/elegantwizardry/onlineattendanceapp/AttendanceInfoSubmit.java
@@ -1,13 +1,19 @@
 package com.elegantwizardry.onlineattendanceapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class AttendanceInfoSubmit extends AppCompatActivity implements MyClickListener{
+public class AttendanceInfoSubmit extends AppCompatActivity{
+
+    Button Submit;
+    private final int UPDATE_NAME_REQUEST_CODE = 456;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +21,9 @@ public class AttendanceInfoSubmit extends AppCompatActivity implements MyClickLi
        // setContentView(R.layout.activity_attendance_info_submit);
         setContentView(R.layout.attendance_info_submit);
 
+         Submit = findViewById(R.id.submit);
         EditText name = findViewById(R.id.name_edt);
-       // EditText id = findViewById(R.id.userid_edt);
+        EditText id = findViewById(R.id.userid_edt);
 
         if (getIntent() != null) {
             Stores.Data data = (Stores.Data) getIntent().getSerializableExtra("key_data");
@@ -25,12 +32,30 @@ public class AttendanceInfoSubmit extends AppCompatActivity implements MyClickLi
                 name.setText(data.getName());
             }
         }
+
     }
 
     @Override
-    public void myOnClick(Stores.Data data) {
-        Intent intent = new Intent(this, AttendanceInfoSubmit.class);
-        intent.putExtra("key_data", data);
-        startActivity(intent);
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == UPDATE_NAME_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            String nameUdateSuccess = data.getStringExtra("name_udate_success");
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(AttendanceInfoSubmit.this, AttendanceSubmission.class);
+                startActivityForResult(intent, UPDATE_NAME_REQUEST_CODE);
+            }
+        });
     }
 }
--- app/src/main/java/com/elegantwizardry/onlineattendanceapp/AttendanceSubmission.java
+++ app/src/main/java/com/elegantwizardry/onlineattendanceapp/AttendanceSubmission.java
@@ -1,14 +1,42 @@
 package com.elegantwizardry.onlineattendanceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class AttendanceSubmission extends AppCompatActivity {
+public class AttendanceSubmission extends AppCompatActivity{
+
+    TextView name;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance_submission);
+
+        name = findViewById(R.id.name);
+
+
+        StoresViewModel storesViewModel = ViewModelProviders.of(this).get(StoresViewModel.class);
+        storesViewModel.sViewModel();
+
+        storesViewModel.getStoresMutableLiveData().observe(this, stores -> {
+            if(stores != null && stores.getDataList() != null){
+                Log.d("name","name coming");
+                name.setText(stores.getDataList().get(0).getName());
+                Intent intent = new Intent();
+                intent.putExtra("name_update_success", "Name changed Successfully");
+                setResult(RESULT_OK, intent);
+            }
+
+        });
     }
 }
+
